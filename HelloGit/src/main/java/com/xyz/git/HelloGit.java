2555f56256995651b5c281598316553aa5c136e3
Cyb-xyz <519727964@qq.com>
2022-03-30 10:43:19 +0800

    添加文件(20220330)

---
+++ HelloGit/src/main/java/com/xyz/git/HelloGit.java
@@ -0,0 +1,16 @@
+package com.xyz.git;
+
+/**
+ * 描述
+ *
+ * @author 小宇宙
+ * @version 1.0
+ * @date 2022/03/30 10:26:43
+ */
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+
+}
